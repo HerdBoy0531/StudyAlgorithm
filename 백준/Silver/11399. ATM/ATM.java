c8b2c5a6c2cf009dd7e988e5cb1ee517a19f4634
HerdBoy0531 <herdboy0531@gmail.com>
2025-08-23 20:13:21 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 15136 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] Time = new int[N];
+		String[] arr = br.readLine().split(" ");
+		
+		for(int i = 0; i < N; i++) {
+			Time[i] = Integer.parseInt(arr[i]);
+		}
+		
+		Arrays.sort(Time);
+		
+		int res = 0;
+		for(int i = 0; i < N; i++) {
+			for(int ii = 0; ii <= i ; ii++) {
+				res += Time[ii];
+			}
+		}
+		
+		bw.write(res+"");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
