da33577273dd97ef162ac32e0bd9c64559588064
HerdBoy0531 <herdboy0531@gmail.com>
2025-09-11 22:21:26 +0900

    [Bronze II] Title: 벨트, Time: 140 ms, Memory: 16428 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.java
+++ 백준/Bronze/10834. 벨트/벨트.java
@@ -3,26 +3,24 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		int cnt = 1;
-		int LR_rotate = 0;
-		
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int L = Integer.parseInt(st.nextToken());
-			int R = Integer.parseInt(st.nextToken());
-			int rotate = Integer.parseInt(st.nextToken());
-			
-			cnt = cnt * R / L;
-			LR_rotate = Math.abs(LR_rotate-rotate);
-		}
-		System.out.println(LR_rotate+" "+cnt);
-		
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
+        long cnt = 1;
+        int rotateState = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int rotate = Integer.parseInt(st.nextToken());
+
+            cnt = cnt * R / L;    // 기어비 업데이트
+            rotateState ^= rotate; // 회전 방향 업데이트 (XOR)
+        }
+
+        System.out.println(rotateState + " " + cnt);
+        br.close();
+    }
 }
