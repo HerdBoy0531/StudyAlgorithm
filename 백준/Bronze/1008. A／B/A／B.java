a40071e97a79a5f44ae1d8a10a4ae8699d894aa6
HerdBoy0531 <herdboy0531@gmail.com>
2025-09-15 23:07:32 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double a = Integer.parseInt(st.nextToken());
+		double b = Integer.parseInt(st.nextToken());
+		double c = a / b;
+		System.out.println(c);
+		
+		
+		br.close();
+	}
+
+}
