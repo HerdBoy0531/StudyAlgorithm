d5ed64f53d685e818df5f382ba86c587a30592b7
HerdBoy0531 <herdboy0531@gmail.com>
2026-03-07 15:27:56 +0900

    [Gold IV] Title: 여행 가자, Time: 148 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,71 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, m;
-	static int[][] map;
-	static boolean[] visited;
-	static int start, end;
-	static boolean res;
-	
-	static void dfs(int cur) {
-		if(cur == end) {
-			res = true;
-			return;
-		}
-		
-		for(int i = 0; i < n; i++) {
-			if(map[cur][i] == 1 && !visited[i]) {
-				visited[i] = true;
-				dfs(i);
-			}
-			if(res) return;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		
-		map = new int[n][n];
-		StringTokenizer st;
-		
-		for(int i = 0; i < n ;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		int[] route = new int[m];
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < m; i++) {
-			route[i] = Integer.parseInt(st.nextToken()) - 1;
-		}
-		
-		res = true;
-		
-		for(int i = 0; i < m-1; i++) {
-			start = route[i];
-			end = route[i+1];
-			
-			visited = new boolean[n];
-			visited[start] = true;
-			
-			boolean check = false;
-			res = false;
-			
-			dfs(start);
-			
-			if(!res) {
-				check = true;
-				break;
-			}
-		}
-		
-		if(res) System.out.println("YES");
-		else System.out.println("NO");
-	}
+
+    static int[] parent;
+
+    static int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a != b) parent[b] = a;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        parent = new int[n];
+
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+
+        StringTokenizer st;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < n; j++) {
+                int v = Integer.parseInt(st.nextToken());
+
+                if(v == 1) union(i, j);
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int root = find(Integer.parseInt(st.nextToken()) - 1);
+
+        for(int i = 1; i < m; i++) {
+            int city = Integer.parseInt(st.nextToken()) - 1;
+
+            if(root != find(city)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
 }
