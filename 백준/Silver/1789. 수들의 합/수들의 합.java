ccbeb689fd3b7e1794b62016faefd6b9a079b5f3
HerdBoy0531 <herdboy0531@gmail.com>
2026-03-13 15:02:47 +0900

    [Silver V] Title: 수들의 합, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long s = Long.parseLong(br.readLine());
+		
+		long sum = 0;
+		long i = 1;
+		
+		while(true) {
+			sum += i;
+			
+			if(sum > s) {
+				System.out.println(i - 1);
+				break;
+			}
+			
+			i++;
+		}
+	}
+}
