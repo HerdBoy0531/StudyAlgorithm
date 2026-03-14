8133453ed3fc1d87b53afff7f8219f71fe314ec5
HerdBoy0531 <herdboy0531@gmail.com>
2026-03-14 15:05:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 304 ms, Memory: 26684 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(
+				(a, b) ->  {
+					if(Math.abs(a) == Math.abs(b))
+						return a - b;
+					return Math.abs(a) - Math.abs(b);
+				}
+		);
+		
+		for(int i = 0; i < n; i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x == 0) {
+				if(pq.isEmpty()) sb.append(0+"\n");
+				else sb.append(pq.poll()+"\n");
+			} else {
+				pq.add(x);
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
