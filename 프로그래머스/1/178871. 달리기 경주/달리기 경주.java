e0f07014c72b8bd77051d1a12db8a6b0fb47e7ed
HerdBoy0531 <herdboy0531@gmail.com>
2026-04-25 23:25:46 +0900

    [level 1] Title: 달리기 경주, Time: 407.15 ms, Memory: 370 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+
+ 
+        for (String name : callings) {
+            int idx = map.get(name);
+
+            String frontPlayer = players[idx - 1];
+
+            players[idx - 1] = name;
+            players[idx] = frontPlayer;
+
+            map.put(name, idx - 1);
+            map.put(frontPlayer, idx);
+        }
+
+        return players;
+    }
+}
