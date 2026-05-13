cef9806de255128aacb56166ca6e680c1d97a3ae
HerdBoy0531 <herdboy0531@gmail.com>
2026-05-13 12:02:24 +0900

    Time: 6 ms (100%), Space: 76.7 MB (23.29%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int low = Math.min(a, b) + 1;
+            int high = Math.max(a, b) + limit;
+            int sum = a + b;
+
+            diff[low] -= 1;
+
+            diff[sum] -= 1;
+            diff[sum + 1] += 1;
+
+            diff[high + 1] += 1;
+        }
+
+        int answer = Integer.MAX_VALUE;
+
+        int current = n;
+
+        for (int target = 2; target <= 2 * limit; target++) {
+            current += diff[target];
+            answer = Math.min(answer, current);
+        }
+
+        return answer;
+    }
+}
