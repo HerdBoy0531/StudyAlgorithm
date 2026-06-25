603a0bb9721f435044a3929008b39f6f0b37ee24
HerdBoy0531 <herdboy0531@gmail.com>
2026-06-25 11:48:31 +0900

    Time: 12 ms (90.15%), Space: 47.5 MB (8.92%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+
+        long[] prefix = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        long[] compress = prefix.clone();
+        Arrays.sort(compress);
+
+        Map<Long, Integer> rank = new HashMap<>();
+        int idx = 1;
+
+        for (long v : compress) {
+            if (!rank.containsKey(v)) {
+                rank.put(v, idx++);
+            }
+        }
+
+        Fenwick bit = new Fenwick(idx);
+
+        long answer = 0;
+
+        for (long cur : prefix) {
+            int r = rank.get(cur);
+
+            // prefix[i] < prefix[j]
+            answer += bit.query(r - 1);
+
+            bit.update(r, 1);
+        }
+
+        return (int) answer;
+    }
+
+    static class Fenwick {
+        long[] tree;
+
+        Fenwick(int n) {
+            tree = new long[n + 1];
+        }
+
+        void update(int idx, long val) {
+            while (idx < tree.length) {
+                tree[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        long query(int idx) {
+            long sum = 0;
+
+            while (idx > 0) {
+                sum += tree[idx];
+                idx -= idx & -idx;
+            }
+
+            return sum;
+        }
+    }
+}
